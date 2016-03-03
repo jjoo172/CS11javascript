ed3f59f5f07693babdf7004a87d052b9515d0110
jonathan <joo@caltech.edu>
2016-03-02 22:49:10 -0800

    didn't break anything yet

--- hw5/tables2.js
+++ hw5/tables2.js
@@ -12,24 +12,16 @@ var SortTable = function(id, types) {
    *   have been globals before)
    */
 
-  this.cur_sorted_column = -1;
-  this.sort_direction = 1; // indicates the currently sorted direction of the column.
-
-  this.iden = id;
-  this.types = types;
-
-  alert("asfd");
-  alert(this.iden);
-
-  this.table = document.getElementById(this.iden);
-  this.heads = this.table.getElementsByTagName("thead");
-  alert(this.heads[0]);
-  this.header_labels = this.heads[0].getElementsByTagName("th");
-
   window.addEventListener("load", function(e) {
-    for(var i = 0; i < this.header_labels.length; i++) {
-      alert(this.header_labels[i].innerHTML);
-      this.header_labels[i].addEventListener("click", (function(idx) {
+
+    var table = document.getElementById(id);
+    var headers = table.getElementsByTagName("thead");
+    alert(headers[0]);
+    var header_labels = headers[0].getElementsByTagName("th");
+
+    for(var i = 0; i < header_labels.length; i++) {
+      alert(header_labels[i].innerHTML);
+      header_labels[i].addEventListener("click", (function(idx) {
         return function(e) {
           this.prototype.sortColumn(idx);
         }
@@ -40,7 +32,8 @@ var SortTable = function(id, types) {
 
 };
 
-SortTable.prototype.sortColumn = function(index) {  
+SortTable.prototype.sortColumn = function(index) {
+  "use strict";
   /*
    * TODO: Complete this function.
    *
@@ -49,160 +42,4 @@ SortTable.prototype.sortColumn = function(index) {
    * got in the constructor to convert from the string values that the cells
    * hold.
    */
-   /*
-   "use strict";
-  
-  var table = document.getElementById("ourtable");
-  var i;
-  var headers = table.getElementsByTagName("th");
-  var elements = table;
-
-  var new_array = [];
-
-  for (var i = 1; i < table.rows.length; i++) {
-    new_array.push([i, table.rows[i].cells[column].innerHTML]);
-  }
-
-  // ascending -> descending
-  if (sort_direction === 1) {
-    new_array.sort(function(a, b) {
-      if (a[1] > b[1]) {
-       return -1;
-      }
-
-      else if (a[1] < b[1]) {
-        return 1;
-      }
-
-      else { 
-        return 0;
-      }
-    });
-  }
-
-  // descending -> ascending
-  else if (sort_direction === -1) {
-    new_array.sort(function(a, b) {
-      if (a[1] > b[1]) {
-       return 1;
-      }
-
-      else if (a[1] < b[1]) {
-        return -1;
-      }
-
-      else { 
-        return 0;
-      }
-    });
-  }
-
-  else {
-    alert("something went wrong!");
-  }
-
-  sort_direction *= -1;
-
-  // at this point, sort_direction holds now the currently sorted direction of new_array
-
-  // column is already sorted
-  if (cur_sorted_column === column) {
-
-    // ascending
-    if (sort_direction === 1) {
-      headers[column].style.color = 'blue';
-      
-      // append new rows
-      for (var i = 0; i < new_array.length; i++) {
-        var row = table.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-
-        cell1.innerHTML = table.rows[new_array[i][0]].cells[0].innerHTML;
-        cell2.innerHTML = table.rows[new_array[i][0]].cells[1].innerHTML;
-        cell3.innerHTML = table.rows[new_array[i][0]].cells[2].innerHTML;
-      }
-
-      // get rid of old rows
-      for (var i = 0; i < new_array.length; i++) {
-        table.deleteRow(1);
-      }
-      
-    }
-    // descending
-    else {
-      headers[column].style.color = 'red';
-      
-      // append new rows
-      for (var i = 0; i < new_array.length; i++) {
-        var row = table.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-
-        cell1.innerHTML = table.rows[new_array[i][0]].cells[0].innerHTML;
-        cell2.innerHTML = table.rows[new_array[i][0]].cells[1].innerHTML;
-        cell3.innerHTML = table.rows[new_array[i][0]].cells[2].innerHTML;
-      }
-
-      // get rid of old rows
-      for (var i = 0; i < new_array.length; i++) {
-        table.deleteRow(1);
-      }
-      
-    }
-  }
-
-  // chosen column is not already sorted
-  else {
-
-    // change color back to black
-    if (cur_sorted_column != -1) {
-      headers[cur_sorted_column].style.color = '';
-    }
-    
-    // sort currently sorted column in ascending.
-    cur_sorted_column = column;
-
-    new_array.sort(function(a, b) {
-      if (a[1] > b[1]) {
-       return 1;
-      }
-
-      else if (a[1] < b[1]) {
-        return -1;
-      }
-
-      else { 
-        return 0;
-      }
-    });
-
-    headers[column].style.color = 'blue';
-
-    // populate actual values
-    
-      // append new rows
-      for (var i = 0; i < new_array.length; i++) {
-        var row = table.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-
-        cell1.innerHTML = table.rows[new_array[i][0]].cells[0].innerHTML;
-        cell2.innerHTML = table.rows[new_array[i][0]].cells[1].innerHTML;
-        cell3.innerHTML = table.rows[new_array[i][0]].cells[2].innerHTML;
-      }
-
-      // get rid of old rows
-      for (var i = 0; i < new_array.length; i++) {
-        table.deleteRow(1);
-      }
-    
-
-    sort_direction = 1;
-
-  }
-  */
 };
